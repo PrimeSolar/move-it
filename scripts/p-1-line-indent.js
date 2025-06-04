b92b8efcada612bd6f7ba06b84775ba2f5870286
I am <88476416+PrimeSolar@users.noreply.github.com>
2025-06-04 10:24:29 +0300

    Update p-1-line-indent.js

--- scripts/p-1-line-indent.js
+++ scripts/p-1-line-indent.js
@@ -1,6 +1,8 @@
-const p = document.getElementsByTagName("p");
-for (let x of p) {
-  if (x.closest(".text-center") == null) {
-    x.innerHTML = "&nbsp;&nbsp;" + x.innerHTML;
+document.addEventListener("DOMContentLoaded", () => {
+  const p = document.getElementsByTagName("p");
+  for (let x of p) {
+    if (x.closest(".text-center") == null) {
+      x.innerHTML = "&nbsp;&nbsp;" + x.innerHTML;
+    }
   }
-}
+});
