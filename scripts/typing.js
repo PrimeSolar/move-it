08f86d185d3e13b3b8faee3db58fb0890fd2c39c
I am <88476416+PrimeSolar@users.noreply.github.com>
2025-10-26 20:56:06 +0300

    Update typing.js

--- scripts/typing.js
+++ scripts/typing.js
@@ -1,3 +1,21 @@
+/*
+ * Typing Animation Script
+ *
+ * This script provides a typing animation effect for elements with a specific class on a webpage,
+ * showing the appearance of text being typed out character by character.
+ *
+ * Copyright © Vladislav Kazantsev
+ * All rights reserved.
+ * This code is the intellectual property of Vladislav Kazantsev.
+ * You are welcome to clone the related repository and use the code for exploratory purposes.
+ * However, unauthorized reproduction, modification, or redistribution of this code (including cloning of related repository or altering it for activities beyond exploratory use) is strictly prohibited.
+ * Code snippets may be shared only when the original author is explicitly credited and a direct link to the original source of the code is provided alongside the code snippet.
+ * Sharing the link to the file is permitted, except when directed toward retrieval purposes.
+ * Any form of interaction with this file is strictly prohibited when facilitated by the code, except when such interaction is for discussion or exchange purposes with others.
+ * This copyright notice applies globally.
+ * For inquiries about collaboration, usage outside exploratory purposes, or permissions, please contact: hypervisor7@pm.me
+ */
+
 document.addEventListener("DOMContentLoaded", () => {
   const TYPING_VELOCITY = 100; // Velocity of typing in milliseconds per character
   const TYPING_CLASS = "typing";
